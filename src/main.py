36a7b8dcc6b0c71dbbd9868e9835c013262d9ea1
Rafael Takahashi <rafael.takahashi20@outlook.com>
2025-01-16 15:58:13 -0300

    refactor: organize files

--- src/main.py
+++ src/main.py
@@ -1,69 +1,12 @@
-import trimesh
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from definitions import projection_center, p1, p2, p3
+from obj import get_obj_data
 
-def get_obj_data(file_path):
-    # Load the .obj file using trimesh
-    mesh = trimesh.load_mesh(file_path)
+print(projection_center)
 
-    # Get object data
-    vertices = mesh.vertices
-    vertice_quantity = len(vertices)
-    faces = mesh.faces
-    face_quantity = len(faces)
-    vertices_per_faces = [len(face) for face in faces]
-    face_vertices = [vertice for vertice in faces]
-
-    print(f"NV: {vertice_quantity}")
-    print("========================================")
-    print(f"X[i], Y[i], Z[i]: \n {vertices}")
-    print("========================================")
-    #print(f" {faces}")
-    #print("========================================")
-    print(f"NS: {face_quantity}")
-    print("========================================")
-    print(f"NVPS[i]: {vertices_per_faces}")
-    print("========================================")
-    for i in range(face_quantity):
-        print(f"VS[{i}]: {face_vertices[i]}")
+print(p1)
+print(p2)
+print(p3)
 
 get_obj_data('models/cube.obj')
-
-projection_center = [1, -1, 1]
-
-p1 = [1, 0, 0]
-p2 = [0, 1, 0]
-p3 = [0, 0, 1]
-
-def plot_3d_obj(file_path):
-    # Load the .obj file using trimesh
-    mesh = trimesh.load_mesh(file_path)
-
-    # Get object data
-    vertices = mesh.vertices
-    faces = mesh.faces
-
-    # Create a 3D plot
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    # Create the 3D mesh using the faces and vertices
-    mesh_faces = []
-    for face in faces:
-        mesh_faces.append([vertices[vertex] for vertex in face])
-
-    # Plot the object
-    ax.add_collection3d(Poly3DCollection(mesh_faces, facecolors='cyan', linewidths=1, edgecolors='r', alpha=.25))
-    
-    # Set plot limits
-    ax.set_xlim([vertices[:, 0].min(), vertices[:, 0].max()])
-    ax.set_ylim([vertices[:, 1].min(), vertices[:, 1].max()])
-    ax.set_zlim([vertices[:, 2].min(), vertices[:, 2].max()])
-
-    # Labels and title
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
-    ax.set_title('.obj Plot')
-
-    #plt.show()
---
+++ src/obj.py
@@ -0,0 +1,26 @@
+import trimesh
+
+def get_obj_data(file_path):
+    # Load the .obj file using trimesh
+    mesh = trimesh.load_mesh(file_path)
+
+    # Get object data
+    vertices = mesh.vertices
+    vertice_quantity = len(vertices)
+    faces = mesh.faces
+    face_quantity = len(faces)
+    vertices_per_faces = [len(face) for face in faces]
+    face_vertices = [vertice for vertice in faces]
+
+    print(f"NV: {vertice_quantity}")
+    print("========================================")
+    print(f"X[i], Y[i], Z[i]: \n {vertices}")
+    print("========================================")
+    #print(f" {faces}")
+    #print("========================================")
+    print(f"NS: {face_quantity}")
+    print("========================================")
+    print(f"NVPS[i]: {vertices_per_faces}")
+    print("========================================")
+    for i in range(face_quantity):
+        print(f"VS[{i}]: {face_vertices[i]}")
